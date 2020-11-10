e8edf2357f50929733c4dd83f7406e7c25ce64b4
Bruxellesflorian <73053109+Bruxellesflorian@users.noreply.github.com>
2020-11-10 10:27:03 +0100

    Delete Class.js

--- Class.js
+++
@@ -1,9 +0,0 @@
-class guerrier {
-  constructor(name, question, reponse) {
-    this.name = name;
-    this.question = question;
-    this.reponse = reponse;
-  }
-}
-var flo = new guerrier("olivier","alo ?","ok");
-console.log(flo.name)
